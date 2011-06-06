acd26ba6dbd4122ddc25c1314b71b5473f4be71d
adonaac <adonaac@gmail.com>
2011-06-06 05:41:54 +0000

    a

--- t.cpp
+++ t.cpp
@@ -27,17 +27,23 @@ typedef struct {
     trianglee *triangle;
 } sector;
 
+typedef struct {
+    GLdouble atx, aty, atz;
+    GLdouble tox, toy, toz;
+    GLfloat xd, yd, zd;
+    GLfloat yaw, pitch;
+} camera;
+
 SDL_Surface *surface;
+camera camera1, camera2;
 
 int light = FALSE;
 int blend = FALSE;
+int ratio;
 
-sector sector1;
-
-GLfloat yrot;
+float speed = 0.01f;
+GLfloat xrot, yrot;
 GLfloat xpos, zpos;
-GLfloat walkbias, walkbiasangle;
-GLfloat lookupdown;
 
 GLfloat LightAmbient[] = {0.5f, 0.5f, 0.5f, 1.0f};
 GLfloat LightDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -46,44 +52,144 @@ GLfloat LightPosition[] = {0.0f, 0.0f, 2.0f, 1.0f};
 const float piover180 = 0.0174532925f;
 
 GLuint filter;
-GLuint texture[3];
+GLuint texture[6];
+GLuint box;
 
 void quit(int returnCode) {
     SDL_Quit();
-    if (sector1.triangle) free(sector1.triangle);
+    // if (sector1.triangle) free(sector1.triangle);
     exit(returnCode);
 }
+
+GLvoid buildLists() {
+    box = glGenLists(1);
+
+    glNewList(box, GL_COMPILE);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+    glEnd();
+    glEndList();
+}
+        
 
 int loadGLTextures() {
     int status = FALSE;
 
-    SDL_Surface *textureImage[1];
+    SDL_Surface *textureImage[6];
 
     if ((textureImage[0] = SDL_LoadBMP("1.bmp"))) {
         status = TRUE;
         
-        glGenTextures(3, &texture[0]);
+        glGenTextures(1, &texture[0]);
         glBindTexture(GL_TEXTURE_2D, texture[0]);
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, textureImage[0]->w, textureImage[0]->h,
-                    0, GL_BGR, GL_UNSIGNED_BYTE, textureImage[0]->pixels);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        glBindTexture(GL_TEXTURE_2D, texture[1]);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, textureImage[0]->w, textureImage[0]->h, 
-                    0, GL_BGR, GL_UNSIGNED_BYTE, textureImage[0]->pixels);
-        glBindTexture(GL_TEXTURE_2D, texture[2]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[0]->w, textureImage[0]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[0]->pixels);    
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[0]->w, textureImage[0]->h,
-                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[0]->pixels);    
     }
 
+    if ((textureImage[1] = SDL_LoadBMP("top.bmp"))) {
+        status = TRUE;
+
+        glGenTextures(1, &texture[1]);
+        glBindTexture(GL_TEXTURE_2D, texture[1]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[1]->w, textureImage[1]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[1]->pixels);    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        
+    }
+
+    if ((textureImage[2] = SDL_LoadBMP("right.bmp"))) {
+        status = TRUE;
+
+        glGenTextures(1, &texture[2]);
+        glBindTexture(GL_TEXTURE_2D, texture[2]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[2]->w, textureImage[2]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[2]->pixels);    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    } 
+
+    if ((textureImage[3] = SDL_LoadBMP("left.bmp"))) {
+        status = TRUE;
+
+        glGenTextures(1, &texture[3]);
+        glBindTexture(GL_TEXTURE_2D, texture[3]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[3]->w, textureImage[3]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[3]->pixels);    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    } 
+
+
+    if ((textureImage[4] = SDL_LoadBMP("front.bmp"))) {
+        status = TRUE;
+
+        glGenTextures(1, &texture[4]);
+        glBindTexture(GL_TEXTURE_2D, texture[4]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[4]->w, textureImage[4]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[4]->pixels);    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    } 
+
+    if ((textureImage[5] = SDL_LoadBMP("back.bmp"))) {
+        status = TRUE;
+
+        glGenTextures(1, &texture[5]);
+        glBindTexture(GL_TEXTURE_2D, texture[5]);
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, textureImage[5]->w, textureImage[5]->h,
+                        GL_BGR, GL_UNSIGNED_BYTE, textureImage[5]->pixels);    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    } 
+
     if (textureImage[0]) SDL_FreeSurface(textureImage[0]);
+    if (textureImage[1]) SDL_FreeSurface(textureImage[1]);
+    if (textureImage[2]) SDL_FreeSurface(textureImage[2]);
+    if (textureImage[3]) SDL_FreeSurface(textureImage[3]);
+    if (textureImage[4]) SDL_FreeSurface(textureImage[4]);
+    if (textureImage[5]) SDL_FreeSurface(textureImage[5]);
 
     return status;
 }
@@ -94,7 +200,7 @@ void readstr(FILE *f, char *string) {
     } while ((string[0] == '/') ||(string[0] == '\n'));
 }
 
-void setupWorld(char *worldFile) {
+/* void setupWorld(char *worldFile) {
     FILE *fileIn;
     int nTriangles, triLoop, verLoop;
     char oneLine[255];
@@ -124,23 +230,86 @@ void setupWorld(char *worldFile) {
     }
 
     fclose(fileIn);
+} */
+
+void renderSky() {
+    glPushMatrix();
+    glLoadIdentity();
+    gluLookAt(0, 0, 0, camera1.atx + camera1.xd, camera1.aty + camera1.yd, camera1.atz + camera1.zd, 0, 1, 0);
+    glDisable(GL_DEPTH_TEST);
+
+    // front
+    glBindTexture(GL_TEXTURE_2D, texture[4]);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f(  0.5f, -0.5f, -0.5f );
+        glTexCoord2f(1, 1); glVertex3f( -0.5f, -0.5f, -0.5f );
+        glTexCoord2f(1, 0); glVertex3f( -0.5f,  0.5f, -0.5f );
+        glTexCoord2f(0, 0); glVertex3f(  0.5f,  0.5f, -0.5f );
+    glEnd();
+ 
+    // left
+    glBindTexture(GL_TEXTURE_2D, texture[3]);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f(  0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 1); glVertex3f(  0.5f, -0.5f, -0.5f );
+        glTexCoord2f(1, 0); glVertex3f(  0.5f,  0.5f, -0.5f );
+        glTexCoord2f(0, 0); glVertex3f(  0.5f,  0.5f,  0.5f );
+    glEnd();
+ 
+    // back
+    glBindTexture(GL_TEXTURE_2D, texture[5]); 
+    glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f( -0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 1); glVertex3f(  0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 0); glVertex3f(  0.5f,  0.5f,  0.5f );
+        glTexCoord2f(0, 0); glVertex3f( -0.5f,  0.5f,  0.5f );
+    glEnd();
+ 
+    // right
+    glBindTexture(GL_TEXTURE_2D, texture[2]);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f( -0.5f, -0.5f, -0.5f );
+        glTexCoord2f(1, 1); glVertex3f( -0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 0); glVertex3f( -0.5f,  0.5f,  0.5f );
+        glTexCoord2f(0, 0); glVertex3f( -0.5f,  0.5f, -0.5f );
+    glEnd();
+ 
+    // top
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1, 1); glVertex3f( -0.5f,  0.5f, -0.5f );
+        glTexCoord2f(1, 0); glVertex3f( -0.5f,  0.5f,  0.5f );
+        glTexCoord2f(0, 0); glVertex3f(  0.5f,  0.5f,  0.5f );
+        glTexCoord2f(0, 1); glVertex3f(  0.5f,  0.5f, -0.5f );
+    glEnd();
+ 
+    // bottom
+    glBindTexture(GL_TEXTURE_2D, texture[1]); 
+    glBegin(GL_QUADS);
+        glTexCoord2f(0, 0); glVertex3f( -0.5f, -0.5f, -0.5f );
+        glTexCoord2f(0, 1); glVertex3f( -0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 1); glVertex3f(  0.5f, -0.5f,  0.5f );
+        glTexCoord2f(1, 0); glVertex3f(  0.5f, -0.5f, -0.5f );
+    glEnd();
+
+    glEnable(GL_DEPTH_TEST);
+    glPopMatrix();
 }
 
 int resizeWindow(int w, int h) {
     if (h == 0) h = 1;
 
-    GLfloat ratio;
     ratio = (GLfloat)w/(GLfloat)h;
 
     glViewport(0, 0, (GLint)w, (GLint)h); 
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+    gluPerspective(90.0f, ratio, 0.1f, 500.0f);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    
+
     return TRUE;
 }
 
@@ -164,22 +333,40 @@ void handleKeyPress(SDL_keysym *keysym) {
             if (!light) glDisable(GL_LIGHTING);
             else glEnable(GL_LIGHTING); break;
 
+        case SDLK_w: 
+            camera1.atx += camera1.xd * speed;
+            camera1.aty += camera1.yd * speed;
+            camera1.atz += camera1.zd * speed;
+            break;
+
+        case SDLK_s:  
+            camera1.atx -= camera1.xd * speed;
+            camera1.aty -= camera1.yd * speed;
+            camera1.atz -= camera1.zd * speed;
+            break;
+
         case SDLK_UP:
-            xpos -= (float)sin(yrot*piover180)*0.005f;
-            zpos -= (float)cos(yrot*piover180)*0.005f;
-            if (walkbiasangle >= 359.0f) walkbiasangle = 0.0f;
-            else walkbiasangle += 0.001f;
-            walkbias = (float)sin(walkbiasangle*piover180)/20.0f; break;
+            camera1.pitch += 0.001f;
+            camera1.yd = atan(tan(atan(camera1.pitch)));
+            break;
 
         case SDLK_DOWN:
-            xpos += (float)sin(yrot*piover180)*0.005f;
-            zpos += (float)cos(yrot*piover180)*0.005f;
-            if (walkbiasangle <= 1.0f) walkbiasangle = 359.0f;
-            else walkbiasangle -= 0.001f;
-            walkbias = (float)sin(walkbiasangle*piover180)/20.0f; break;
+            camera1.pitch -= 0.001f;
+            camera1.yd = atan(tan(atan(camera1.pitch)));
+            break;
 
-        case SDLK_RIGHT: yrot -= 0.015f; break;
-        case SDLK_LEFT: yrot += 0.015f; break;
+        case SDLK_RIGHT: 
+            camera1.yaw += 0.001f;
+            camera1.xd = sin(camera1.yaw);
+            camera1.zd = -cos(camera1.yaw); 
+            break;
+
+        case SDLK_LEFT: 
+            camera1.yaw -= 0.001f; 
+            camera1.xd = sin(camera1.yaw);
+            camera1.zd = -cos(camera1.yaw);
+            break;
+
         case SDLK_F1: SDL_WM_ToggleFullScreen(surface); break;
         default: break;
     }
@@ -188,23 +375,27 @@ void handleKeyPress(SDL_keysym *keysym) {
 int initGL() {
     if (!loadGLTextures()) return FALSE;
 
+    buildLists();
     glEnable(GL_TEXTURE_2D);
     glShadeModel(GL_SMOOTH);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClearDepth(1.0f);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_COLOR_MATERIAL);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
     glLightfv(GL_LIGHT1, GL_POSITION, LightPosition);
-    glEnable(GL_LIGHT1);
     glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    lookupdown = 0.0f;
-    walkbias = 0.0f;
-    walkbiasangle = 0.0f;
+    camera1.atx = camera1.aty = camera1.atz = 0.0f;
+    camera1.tox = camera1.toy = camera1.toz = 0.0f;
+    camera1.xd = camera1.yd = camera1.zd = 0.0f;
+    camera1.yaw = camera1.pitch = 0.0f;
 
     return TRUE;
 }
@@ -212,23 +403,29 @@ int initGL() {
 int drawGLScene() {
     static GLint TO = 0;
     static GLint frames = 0;
-    int loop_m;
-    
-    GLfloat x_m, y_m, z_m, u_m, v_m;
-    GLfloat xtrans = -xpos;
-    GLfloat ztrans = -zpos;
-    GLfloat ytrans = -walkbias - 0.25f;
-    GLfloat sceneroty = 360.0f - yrot;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glLoadIdentity();
-    glRotatef(lookupdown, 1.0f, 0.0f, 0.0f);
-    glRotatef(sceneroty, 0.0f, 1.0f, 0.0f);
-    glTranslatef(xtrans, ytrans, ztrans);
-    glBindTexture(GL_TEXTURE_2D, texture[filter]);
 
-    for (loop_m = 0; loop_m < sector1.nTriangles; loop_m++) {
+    renderSky();
+
+    gluLookAt(camera1.atx, camera1.aty, camera1.atz, camera1.atx + camera1.xd,
+            camera1.aty + camera1.yd, camera1.atz + camera1.zd, 0, 1, 0);
+
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    glTranslatef(0.0f, -10.0f, 0.0f);
+
+    for (int i = 0; i < 60; i++) {
+        glTranslatef(2.0f, 0.0f, 0.0f);
+        glCallList(box); 
+        for (int j = 0; j < 60; j++) {
+            glTranslatef(0.0f, 0.0f, 2.0f);
+            glCallList(box); 
+        }
+        glTranslatef(0.0f, 0.0f, -120.0f);
+    }
+    
+    /* for (loop_m = 0; loop_m < sector1.nTriangles; loop_m++) {
         glBegin(GL_TRIANGLES);
             glNormal3f(0.0f, 0.0f, 1.0f);
             x_m = sector1.triangle[loop_m].vertex[0].x;
@@ -255,7 +452,7 @@ int drawGLScene() {
             glTexCoord2f(u_m, v_m);
             glVertex3f(x_m, y_m, z_m);
         glEnd();
-    }
+    } */
     
     SDL_GL_SwapBuffers();
 
@@ -265,7 +462,7 @@ int drawGLScene() {
         if (t - TO >= 5000) {
             GLfloat seconds = (t-TO)/1000.0;
             GLfloat fps = frames/seconds;
-            printf("%d frames in %g seconds = %g FPS\n", frames, seconds, fps);
+            printf("%d frames in %g seconds = %g FPS, Yaw: %2.4f, Pitch: %2.4f, yd: %2.4f, speed: %2.4f\n", frames, seconds, fps, camera1.yaw, camera1.pitch, camera1.yd, speed);
             TO = t;
             frames = 0;
         }
@@ -309,8 +506,11 @@ int main(int argc, char **argv) {
         quit(1);
     }
 
+    SDL_ShowCursor(0);
+    SDL_WM_GrabInput(SDL_GRAB_ON);
+
     initGL();
-    setupWorld("world.txt");
+    // setupWorld("world.txt");
     resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     while (!done) {
